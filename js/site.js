a4290b52f964644af745959fa7ec0c5645ed6df3
Matthew McMillion <matthew@bitpivot.com>
2013-03-03 23:06:08 -0600

    Fresh empty directory for Rails Vagrant box setup.

--- js/site.js
+++
@@ -1,7 +0,0 @@
-//@code-prepend "jquery-1.9.1.min.js";
-
-//@code-prepend "jquery-rotate.2.2.min.js";
-//@code-prepend "jquery-easing.1.3.min.js";
-
-//@code-prepend "analytics.js";
-//@code-prepend "animate-banner.js";
