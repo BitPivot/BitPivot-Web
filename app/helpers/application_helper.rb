bfe9ccb5e1074dd63da7218be9d8cdb15f0048e5
Matthew McMillion <matthew@bitpivot.com>
2013-04-12 02:13:09 -0500

    Oh crap, responsive banner with text. Sass rules.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  # Sets (if passed arguemnt) or gets a page title
+  # Sets (if passed argument) or gets a page title
   # Defaults should be set in config/app_config.yml
   #
   def title(new_title = nil)
     if new_title.nil?
-      return content_for?(:title) ? content_for(:title) : APP_CONFIG['default_page_title']
+      content_for?(:title) ? content_for(:title) : APP_CONFIG['default_page_title']
     else
       content_for :title, new_title
     end
@@ -16,7 +16,7 @@ module ApplicationHelper
   #
   def meta_description(new_description = nil)
     if new_description.nil?
-      return content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['default_meta_description']
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['default_meta_description']
     else
       content_for :meta_description, new_description
     end
@@ -27,7 +27,7 @@ module ApplicationHelper
   #
   def meta_keywords(new_keywords = nil)
     if new_keywords.nil?
-      return content_for?(:meta_keywords) ? content_for(:meta_keywords) : APP_CONFIG['default_meta_keywords']
+      content_for?(:meta_keywords) ? content_for(:meta_keywords) : APP_CONFIG['default_meta_keywords']
     else
       content_for :meta_keywords, new_keywords
     end
