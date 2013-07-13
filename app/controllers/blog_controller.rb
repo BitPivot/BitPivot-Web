5007325f85f777a2e1b59a24d2ee52136e259603
Sean Kennedy <cspheregreen@gmail.com>
2013-07-12 21:11:54 -0500

    Add sqlite3 temporarily to remove gemfile error.  Add show_post to route mapped to blog#show_post.  New blog post added."

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,2 +1,9 @@
 class BlogController < ApplicationController
+
+  def show_post
+    filename = "_#{params[:year]}-#{params[:month]}-#{params[:day]}-#{params[:title]}.html.erb"
+    puts "Rendering #{filename}"
+    render "blog/posts/#{filename}"
+  end
+
 end
