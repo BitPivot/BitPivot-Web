fe7d0dbaacc4ab45b009fcd69ab9180d3b168681
Sean Kennedy <cspheregreen@gmail.com>
2013-07-22 18:58:36 -0500

    More expandy stuff.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,62 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.easing.1.3
 
-$(document).on('click', 'section a', function () {
-    var sections = $('#home_controller').find('section');
-    $.each(sections, function (i, section) {
-        var section = $(section);
-        section.removeClass('expanded normal');
-        section.addClass('collapsed');
+$.easing.def = "easeOutQuad";
+
+var sections = [];
+var hNormal = '33.33%';
+var hExpanded = '80%';
+var hCollapsed = '10%';
+var fadeDuration = 2000;
+
+function resize(el, height, duration) {
+    el.animate({height: height}, duration);
+}
+
+function overlay(section, bool) {
+    var o = section.find('.overlay');
+    if (bool) {
+        o.fadeIn(fadeDuration);
+        return;
+    }
+    o.fadeOut(fadeDuration);
+
+}
+
+function expand(i) {
+    for (var ix = 0; ix < sections.length; ix++) {
+        if (ix !== i) collapse(ix);
+    }
+    var s = sections[i];
+
+    overlay(s, false);
+    resize(s, hExpanded, fadeDuration);
+    s.addClass('expanded');
+}
+
+function collapse(i) {
+    console.log(sections);
+    var s = sections[i];
+    overlay(s, true);
+    resize(s, hCollapsed, fadeDuration);
+    s.addClass('collapsed');
+    s.removeClass('normal expanded');
+}
+
+$(document).ready(function () {
+    $.each($('section'), function (i, s) {
+        sections.push($(s));
     });
 
-    var expanded = $(this).parent();
-    expanded.removeClass('collapsed');
-    expanded.addClass('expanded');
+    // Initial animation
+    setTimeout(function () {
+        sections[0].click();
+    }, 2000);
+});
+
+//
+$(document).on('click', '.splash', function () {
+    expand($(this).parent().find('.splash').index($(this)));
 });
