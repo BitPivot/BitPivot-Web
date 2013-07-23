c05264a60686918d44d6327fad5788fed1310a8e
Matthew McMillion <matthew@bitpivot.com>
2013-07-23 15:53:13 -0500

    Implement headers and fading and stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,54 +3,87 @@
 //= require jquery_ujs
 //= require jquery.easing.1.3
 
-// Set jquery default animation easing
+
+
 $.easing.def = "easeInOutCubic";
+var hExpanded = '80%';
+var fadeDuration = 750;
+
+
 
 var sections = [];
-var hExpanded = '70%';
-var fadeDuration = 750;
 var hCollapsed;
+
+function setCollapsedHeaderHeights() {
+    var wrapperHeight = parseInt($('.splash-wrapper').height());
+    var collapsedHeight = (parseInt(hCollapsed) / 100) * wrapperHeight + 'px';
+    $('.splash-banner__header').height(collapsedHeight);
+}
+
+
 
 function resize(element, height, duration) {
     element.animate({height: height}, duration);
 }
 
+function toggleSectionContent(section, expanded) {
+    if (expanded) {
+        section.addClass('expanded');
+        section.find('.splash-banner__header').fadeOut(fadeDuration)
+        section.find('.splash-banner__text').fadeIn(fadeDuration)
+        section.find('.splash-content').fadeIn(fadeDuration)
+    }
+    else {
+        section.removeClass('expanded')
+        section.find('.splash-banner__header').fadeIn(fadeDuration)
+        section.find('.splash-banner__text').fadeOut(fadeDuration)
+        section.find('.splash-content').fadeOut(fadeDuration)
+    }
+}
+
 function expand(i) {
     var section = sections[i];
+
     for (var j = 0; j < sections.length; j++) {
         if (j !== i) collapse(j);
     }
+
     resize(section, hExpanded, fadeDuration);
-    $(section.find('.overlay')).fadeOut(fadeDuration);
-    section.addClass('expanded');
+    toggleSectionContent(section, true);
 }
 
 function collapse(i) {
     var section = sections[i];
     resize(section, hCollapsed, fadeDuration);
-    section.addClass('collapsed');
-    $(section.find('.overlay')).fadeIn(fadeDuration);
-    section.removeClass('normal expanded');
+    toggleSectionContent(section, false);
 }
 
+$(document).on('click', '.splash-banner__header', function () {
+    var parent = $(this).closest('.splash');
+    expand($('.splash').index(parent));
+});
+
+
+
 window.onload = function() {
-    // cache jquery objects
+    // Cache jQuery objects
     $.each($('.splash'), function (i, s) {
         sections.push($(s));
     });
 
     // Calculate hCollapsed base on hExpanded
     hCollapsed = (100 - parseInt(hExpanded)) / (sections.length - 1) + '%'
+    setCollapsedHeaderHeights();
 
     // Initial animation
     setTimeout(function () {
-        sections[0].click();
+        sections[0].find('.splash-banner__header').click();
     }, fadeDuration * 2);
 
     // Fade out loading overlay
     $('#js-loading-overlay').fadeOut(fadeDuration)
 }
 
-$(document).on('click', '.splash', function () {
-    expand($(this).parent().find('.splash').index($(this)));
-});
+window.onresize = function(event) {
+    setCollapsedHeaderHeights();
+}
