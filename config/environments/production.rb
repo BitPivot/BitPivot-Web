2bcabdb77bd014556dcd4a59aa797e1b0cea9c6d
Matthew McMillion <matthew@bitpivot.com>
2013-07-28 11:04:32 -0500

    Testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ BitPivot::Application.configure do
 
 
   # Precompile
-  config.assets.precompile += ['application.js', 'blinds.js', 'blog.js', 'application.css', 'blog.css']
+  #config.assets.precompile += ['application.js', 'blinds.js', 'blog.js', 'application.css', 'blog.css']
 
   # Add Symbolsets to asset pipeline
-  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-  config.assets.precompile += ['*.svg', '*.eot', '*.woff', '*.ttf']
+  #config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+  #config.assets.precompile += ['*.svg', '*.eot', '*.woff', '*.ttf']
 end
