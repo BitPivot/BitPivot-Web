4f2550fa30d440375554759c310f6563ba53fcc3
Sean Kennedy <cspheregreen@gmail.com>
2013-07-27 21:21:27 -0500

    Blog paging.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,6 +3,18 @@ class BlogController < ApplicationController
   layout 'blog'
 
   def view
+    @page_size = 5
+    @page_posts = []
+    @page = Integer(params[:page]) || 1
+    @posts = Dir['app/views/blog/posts/*.erb']
+    @posts.each_with_index do |p, i|
+      if i >= (@page - 1) * @page_size && i < ((@page-1) * @page_size) + @page_size
+        @page_posts.push("/#{p}")
+      end
+    end
+  end
+
+  def show
     render "blog/posts/#{params[:year]}-#{params[:month]}-#{params[:day]}#{params[:title]}"
   end
 
