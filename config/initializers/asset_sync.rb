98d944d4cd3fd6e90d6999d3af23a72c0a05f92d
Matthew McMillion <matthew@bitpivot.com>
2013-07-28 15:50:28 -0500

    Let's see if this works

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -13,9 +13,12 @@ AssetSync.configure do |config|
   # Automatically replace files with their equivalent gzip compressed version
   config.gzip_compression = true
 
+  # Ignored files
+  config.ignored_files = [%r(turbolinks-.*\.js)]
+
   # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
-  config.manifest = true
+  # config.manifest = true
 
   # Fail silently.  Useful for environments such as Heroku
   # config.fail_silently = true
