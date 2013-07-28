7bcc4e6c70012ec7885ee4bf712ba64fd8ee9c59
Matthew McMillion <matthew@bitpivot.com>
2013-07-28 16:01:31 -0500

    MOAR TEST

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,10 +1,8 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  # To use AWS reduced redundancy storage.
-  # config.aws_reduced_redundancy = true
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']         # heroku config:set AWS_ACCESS_KEY_ID
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY'] # heroku config:set AWS_SECRET_ACCESS_KEY
+  config.fog_directory = ENV['FOG_DIRECTORY']                 # heroku config:set FOG_DIRECTORY
 
   # Invalidate a file on a cdn after uploading files
   # config.cdn_distribution_id = "12345"
@@ -14,7 +12,10 @@ AssetSync.configure do |config|
   config.gzip_compression = true
 
   # Ignored files
-  config.ignored_files = [%r(turbolinks-.*\.js)]
+  config.ignored_files = [
+    %r(partials/.*),
+    %r(turbolinks-.*\.js)
+  ]
 
   # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
