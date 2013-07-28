1e735e5adcc570784c70f594f7f93608846f651e
Matthew McMillion <matthew@bitpivot.com>
2013-07-28 16:12:35 -0500

    just more testing

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -5,12 +5,7 @@ AssetSync.configure do |config|
   config.fog_directory = ENV['FOG_DIRECTORY']                 # heroku config:set FOG_DIRECTORY
 
   config.existing_remote_files = 'delete'
-
-  # Invalidate a file on a cdn after uploading files
-  # config.cdn_distribution_id = "12345"
-  # config.invalidate = ['file1.js']
-
-  # Automatically replace files with their equivalent gzip compressed version
+  config.manifest = false
   config.gzip_compression = true
 
   # Ignored files
@@ -18,11 +13,4 @@ AssetSync.configure do |config|
     %r(partials/.*),
     %r(turbolinks-.*\.js)
   ]
-
-  # Use the Rails generated 'manifest.yml' file to produce the list of files to
-  # upload instead of searching the assets directory.
-  # config.manifest = true
-
-  # Fail silently.  Useful for environments such as Heroku
-  # config.fail_silently = true
 end
