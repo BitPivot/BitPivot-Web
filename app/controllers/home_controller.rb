149a66622aa0c51a63f3a4ec07912f693bdae8cf
Sean Kennedy <cspheregreen@gmail.com>
2013-07-29 12:09:42 -0500

    Heroku ordering.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,12 @@ class HomeController < ApplicationController
   def view
     @blinds = []
     paths = Dir['app/views/home/blinds/*.erb']
+    puts "\nPATHS\n\n #{paths}\n\n"
     paths.sort
+    puts "\nPATHS_SORTED\n\n #{paths}\n\n"
     paths.each do |p|
       @blinds.push("/#{p}")
     end
+    puts "\nPATHS_WITH_APPENDED_FORWARDSLASH\n\n#{@blinds}\n\n"
   end
 end
