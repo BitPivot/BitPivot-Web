d63d51f56ac4d0dccae0e2c102a52c2264786bc3
Matthew McMillion <matthew@bitpivot.com>
2013-08-03 19:17:36 -0500

    Add response_to column for comments

--- app/models/blog_post_comment.rb
+++ app/models/blog_post_comment.rb
@@ -1,5 +1,6 @@
 class BlogPostComment < ActiveRecord::Base
-  attr_accessible :author, :email, :votes, :content
+  attr_accessible :author, :email, :votes, :content, :response_to
 
   belongs_to :blog_post
+  has_one :blog_post_comment, :foreign_key => 'response_to'
 end
