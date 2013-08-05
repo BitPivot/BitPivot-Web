7dfefdfcf2fb67800a57a539e08bf77fc6ff459b
Matthew McMillion <matthew@bitpivot.com>
2013-08-05 14:25:12 -0500

    Formatting stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130805042805) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "blog_post_comments", force: true do |t|
     t.integer  "blog_post_id"
     t.integer  "votes"
@@ -24,8 +21,8 @@ ActiveRecord::Schema.define(version: 20130805042805) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                         null: false
-    t.boolean  "approved",      default: false
     t.integer  "respond_to_id"
+    t.boolean  "approved",      default: false
   end
 
   create_table "blog_posts", force: true do |t|
