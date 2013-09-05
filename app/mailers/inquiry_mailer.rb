48663e4d77fb740dadfc780a631eda52d8458a09
Matthew McMillion <matthew@bitpivot.com>
2013-09-05 18:16:44 -0500

    Testing...

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ActionMailer::Base
   default from: 'noreply@bitpivot.com'
 
   def new_inquiry_notification(inquiry)
-    to = 'info@bitpivot.com'
+    to = 'matthew@bitpivot.com'
     subject = "New inquiry from #{inquiry.author} (#{inquiry.email})"
     body = inquiry.content
     mail(to: to, subject: subject, body: body)
